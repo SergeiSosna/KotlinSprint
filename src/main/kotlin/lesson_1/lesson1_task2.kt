f7d5e165036646c7ba60a16c96aa142eb1e93b05
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-06 21:40:03 +0300

    KS-1-2 added employees

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    // выведи в консоль значения всех объявленных ранее переменных
+    // сделать чтобы программа хранила количество работников
+    // один работник уволился
+    val numberOfOrders: Int = 75
+    println(numberOfOrders)
+    val thanksForPurchase: String =
+        "Вместе мы создаем уникальные моменты! Спасибо за ваш заказ! Мы уверены, что выбранный вами товар станет частью особенных моментов. Спасибо за возможность быть с вами в эти важные моменты!"
+    println(thanksForPurchase)
+//    val numberOfEmployees = 2000
+//    println(numberOfEmployees)
+    val numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+}
