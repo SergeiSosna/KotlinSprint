1022e1358d88df21b4c6e153dda90ac12ea9fa02
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-07 19:39:00 +0300

    KS-2-3 train arrival

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val hourDeparture = 9
+    val minutesOfDeparture = 39
+    val pathTime = 457
+    val hoursOnRoad = pathTime / 60
+    val minutesOnRoad = pathTime % 60
+    val hourFromMinutesConversion = (minutesOfDeparture + minutesOnRoad) / 60
+    val minutesOfArrival = (minutesOfDeparture + minutesOnRoad) % 60
+    val arrivalHour = hourDeparture + hoursOnRoad + hourFromMinutesConversion
+    println("Время прибытия поезда: $arrivalHour:$minutesOfArrival")
+
+}
