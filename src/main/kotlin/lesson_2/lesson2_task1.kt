4ab46ac7e642ec5c894a768abd5696b472b63e35
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-07 18:22:37 +0300

    KS-2-1 arithmetic mean

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val numberOfStudents = 4
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+    val arithmeticMean: Float = (student1 + student2 + student3 + student4) / numberOfStudents.toFloat()
+    println(arithmeticMean)
+
+}
