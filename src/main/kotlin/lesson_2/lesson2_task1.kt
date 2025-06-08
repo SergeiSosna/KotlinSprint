ce46b93fbabc61a9c450f307ad514056d85c5e1d
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-08 15:40:50 +0300

    KS-2-1  refactor

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -3,11 +3,14 @@ package org.example.lesson_2
 fun main() {
 
     val numberOfStudents = 4
-    val student1 = 3
-    val student2 = 4
-    val student3 = 3
-    val student4 = 5
-    val arithmeticMean: Float = (student1 + student2 + student3 + student4) / numberOfStudents.toFloat()
-    println(arithmeticMean)
+    val studentScores1 = 3
+    val studentScores2 = 4
+    val studentScores3 = 3
+    val studentScores4 = 5
+
+    val arithmeticMean: Float = (studentScores1 + studentScores2 + studentScores3 + studentScores4) / numberOfStudents.toFloat()
+    val formatedArithmeticMean = String.format("%.2f", arithmeticMean)
+
+    println(formatedArithmeticMean)
 
 }
