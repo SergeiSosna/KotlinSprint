2d4f5b8c8a979bf49125d383fbe4b458015882ab
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-09 20:05:36 +0300

    KS-2-5  refactor bank amount

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -5,12 +5,11 @@ import kotlin.math.pow
 fun main() {
 
     val initialAmount = 70000
-    val interestRate = 0.167
+    val interestRate = 16.7
     val depositTerm = 20
 
-    val finalAmount = initialAmount * (1 + interestRate).pow(depositTerm.toDouble())
-    val formatedFinalAmount = "%.3f".format(finalAmount)
+    val finalAmount = initialAmount * (1 + interestRate / 100).pow(depositTerm.toDouble())
 
-    println(formatedFinalAmount)
+    println("%.3f".format(finalAmount))
 
 }
