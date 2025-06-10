03fefcdd68fc69d4fcc80f86c788b636db77d5cc
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-10 08:28:29 +0300

    KS-3-1 greetings

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var greetings = "Добрый день"
+    val userName = "Анастасия"
+    val char = '!'
+
+    println("$greetings $userName$char")
+
+    greetings = "Добрый вечер"
+
+    println("$greetings $userName$char")
+
+}
