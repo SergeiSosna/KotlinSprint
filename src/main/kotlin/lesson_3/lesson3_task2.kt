1e15290e11f5b376c2773c957fa923744e360989
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-10 08:42:44 +0300

    KS-3-2 personal data

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var surName = "Андреева"
+    val userName = "Татьяна"
+    val patronymic = "Сергеевна"
+    var userAge = 20
+
+    println("$surName $userName $patronymic, $userAge")
+
+    surName = "Сидорова"
+    userAge = 22
+
+    println("$surName $userName $patronymic, $userAge")
+
+}
