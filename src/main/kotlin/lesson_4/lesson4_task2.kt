b40f2cd3c53864e366d65358abb63ea60e2d4a8b
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-11 14:29:04 +0300

    KS-4-2 refactor

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -7,8 +7,13 @@ fun main() {
     val weightCargo2 = 50
     val volumeCargo2 = 100
 
-    println("Груз1 с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории 'Average': ${(WEIGHT_MIN_AVERAGE < weightCargo1) && (weightCargo1 <= WEIGHT_MAX_AVERAGE) && (volumeCargo1 < VOLUME_MAX_AVERAGE)}")
-    println("Груз2 с весом $weightCargo2 кг и объемом $volumeCargo2 л соответствует категории 'Average': ${(WEIGHT_MIN_AVERAGE < weightCargo2) && (weightCargo2 <= WEIGHT_MAX_AVERAGE) && (volumeCargo2 < VOLUME_MAX_AVERAGE)}")
+    val correspondence =(WEIGHT_MIN_AVERAGE < weightCargo1) && (weightCargo1 <= WEIGHT_MAX_AVERAGE)
+            && (volumeCargo1 < VOLUME_MAX_AVERAGE)
+
+    println("Груз1 с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории 'Average':$correspondence")
+    println("Груз2 с весом $weightCargo2 кг и объемом $volumeCargo2 л соответствует категории 'Average': " +
+            "${(WEIGHT_MIN_AVERAGE < weightCargo2) && (weightCargo2 <= WEIGHT_MAX_AVERAGE) 
+                    && (volumeCargo2 < VOLUME_MAX_AVERAGE)}")
 
 }
 
