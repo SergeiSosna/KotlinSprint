f897d7fee3fb603945f13e11998cc4a1440685d1
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-12 13:37:37 +0300

    KS-5-1 refactor

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,18 +1,28 @@
 package org.example.lesson_5
 
+import kotlin.random.Random
+
 fun main() {
 
-    println("""
-        Для входа в приложение нам необходимо проверить, не являетесь ли вы ботом.
-        Для этого Вам необходимо решить следующий пример: 4 + 3 = 
-        Ответ введите в окне ниже.
-    """.trimIndent())
+    val minNumber = 1
+    val maxNumber = 10
+    val randomNumber1 = Random.nextInt(minNumber, maxNumber)
+    val randomNumber2 = Random.nextInt(minNumber, maxNumber)
 
+    println(
+        """
+        Для входа в приложение нам необходимо проверить, не являетесь ли вы ботом.
+        Для этого Вам необходимо решить следующий пример: $randomNumber1 + $randomNumber2 = 
+        Ответ введите в окне ниже.
+    """.trimIndent()
+    )
+
+    val sum = randomNumber1 + randomNumber2
     val answer = readln().toInt()
 
-    if (answer == 7){
+    if (answer == sum) {
         println("Добро пожаловать!")
-    }else{
+    } else {
         println("Доступ запрещен.")
     }
 
