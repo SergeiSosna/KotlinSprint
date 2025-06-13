a1ecdaeeb70fceb52323f46cc55e41aa2b5c4127
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-13 21:56:49 +0300

    KS-5-7 fuel

---
+++ src/main/kotlin/lesson_5/lesson5_task7.kt
@@ -0,0 +1,32 @@
+package org.example.lesson_5
+
+fun main() {
+
+    println(
+        """
+        Приветствуем Вас в приложении Калькулятор топлива.
+        Оно расчитывает стоимость топлива для поездки в зависимости от расхода автомобиля и текущей цены на бензин.
+        Для расчета Вам необходимо ввести некоторые данные (расстояние поездки, расход и стоимость топлива.)
+    """.trimIndent()
+    )
+
+    println("Введите расстояние поездки (в километрах):")
+    val travelDistance = readln().toFloat()
+
+    println("Введите расход топлива на 100 км (в литрах):")
+    val fuelConsumption = readln().toFloat()
+
+    println("Текущую цену за литр топлива:")
+    val fuelCost = readln().toFloat()
+
+    val fuelForTheTrip = (travelDistance * fuelConsumption) / FUEL_FORMAT
+    val totalFuelCost = fuelForTheTrip * fuelCost
+
+    println(
+        "Общее количество топлива:${"%.2f".format(fuelForTheTrip)}.\n" +
+                "Итоговая стоимость поездки:${"%.2f".format(totalFuelCost)}"
+    )
+
+}
+
+const val FUEL_FORMAT = 100
