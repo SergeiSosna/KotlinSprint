61b057796ae555a9f5bdbf23505da7f1d80b86cd
SergeiSosna <sosna.sergei2015@gmail.com>
2025-06-13 17:29:08 +0300

    KS-5-6 index

---
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -0,0 +1,50 @@
+package org.example.lesson_5
+
+fun main() {
+
+    println(
+        """
+        Приветствуем Вас в приложении по расчету Индекса Массы Тела (ИМТ).
+        Для произведения необходимых расчетов в окне ниже введите свой вес и рост.
+        Вес необходимо ввести в киллограмах, а рост в сантиметрах.
+        """.trimIndent()
+    )
+
+    println("Введите вес (в киллограммах):")
+    val userWeight = readln().toFloat()
+
+    println("Введите рост (в сантиметрах):")
+    val userHeight = readln().toFloat()
+
+    val userHeightInMeters = userHeight / CENTIMETERS_IN_A_METER
+
+    val bmi = userWeight / (userHeightInMeters * userHeightInMeters)
+    val bmiResult = if (bmi < BMI_MIN) {
+        "Ваш ИМТ: ${"%.2f".format(bmi)}. Недостаточная масса тела."
+    } else if (bmi >= BMI_MIN && bmi < BMI_AVERAGE) {
+        "Ваш ИМТ: ${"%.2f".format(bmi)}. Нормальная масса тела."
+    } else if (bmi >= BMI_AVERAGE && bmi < BMI_MAX) {
+        "Ваш ИМТ: ${"%.2f".format(bmi)}. Избыточная масса тела."
+    } else if (bmi >= BMI_MAX) {
+        "Ваш ИМТ: ${"%.2f".format(bmi)}. Ожирение."
+    } else {
+        "Неправильное значение."
+    }
+
+    println(bmiResult)
+
+//    val bmiResult = when {
+//        bmi < BMI_MIN -> "Ваш ИМТ: ${"%.2f".format(bmi)}. Недостаточная масса тела."
+//        bmi < BMI_AVERAGE -> "Ваш ИМТ: ${"%.2f".format(bmi)}. Нормальная масса тела."
+//        bmi < BMI_MAX -> "Ваш ИМТ: ${"%.2f".format(bmi)}. Избыточная масса тела."
+//        else -> "Ваш ИМТ: ${"%.2f".format(bmi)}. Ожирение."
+//    }
+//
+//    println(bmiResult)
+
+}
+
+const val CENTIMETERS_IN_A_METER = 100
+const val BMI_MIN = 18.5
+const val BMI_AVERAGE = 25
+const val BMI_MAX = 30
